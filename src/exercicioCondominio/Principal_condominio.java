36e7d317adfc1485d7b9fe6935f34e4720278e9e
EduardoVandresen <eduardovschmoeller@gmail.com>
2023-10-04 22:15:45 -0300

    atividade

---
+++ src/exercicioCondominio/Principal_condominio.java
@@ -0,0 +1,112 @@
+package exercicioCondominio;
+
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+public class Principal_condominio {
+	
+	
+	private static ArrayList <Apartamento> apartamentos = new ArrayList<Apartamento>();
+	private static ArrayList <Despesa> despesas = new ArrayList<Despesa>();
+	
+	
+	public static void main(String[] args) {
+		int op = 0;
+		do {
+			op = menu();
+			switch (op) {
+			case 1:
+			        int numeroAp = Integer.parseInt(JOptionPane.showInputDialog("Informe o número do novo apartamento:"));
+				if (!verificaNumeroAp(numeroAp)) {
+
+                                        apartamentos.add(new Apartamento(numeroAp));
+
+                                        JOptionPane.showMessageDialog(null, "Apartamento cadastrado com sucesso!");
+                                } else {
+                                        JOptionPane.showMessageDialog(null, "O apartamento já existe!");
+                                }
+				break;
+			case 2:
+				despesas.add(new Despesa());
+				break;
+			case 3:
+				Listagem1();
+				break;
+			case 4:
+				Listagem2();
+				break;
+			}
+			}while(op != 5);
+	}
+	public static int menu() {
+		String menu = "1 - Cadastrar Apartamento\n"
+				+ "2 - Cadastrar Despesa\n"
+				+ "3 - Listagem 1\n"
+				+ "4 - Listagem 2\n"
+				+ "5 - Sair";
+		return Integer.parseInt(JOptionPane.showInputDialog(menu));
+	}
+	
+		public static int numeroAp() {
+		int con = 0;
+		for(Apartamento a: apartamentos) {		
+			con ++;
+		}
+		return con;
+	}
+	
+	
+	public static void Listagem1() {
+		Double totaldesp = 0.0;
+		int selecmes;
+		int selecano;
+		selecano = Integer.parseInt(JOptionPane.showInputDialog(null,"Informe o ano"));
+		selecmes = Integer.parseInt(JOptionPane.showInputDialog(null,"Informe o mes"));
+		
+		for(Despesa d: despesas) {
+			if (d.getMes()== selecmes && d.getAno()== selecano){
+				Double valorporap = d.getVal()/numeroAp();
+				totaldesp+=valorporap;
+			}
+		}
+		for(Apartamento a: apartamentos) {
+			Double valortot = a.getVal_fix()+totaldesp;
+			JOptionPane.showMessageDialog(null,"O proprietario "+a.getPropri()+" deve: "+ valortot +"\n");
+		}
+	}
+	public static void Listagem2() {
+		Double totaldesp = 0.0;
+		Double soma = 0.0;
+		int selecmes;
+		int selecano;
+		int selecap;
+		selecano = Integer.parseInt(JOptionPane.showInputDialog(null,"Informe o ano"));
+		selecmes = Integer.parseInt(JOptionPane.showInputDialog(null,"Informe o mes"));
+		selecap = Integer.parseInt(JOptionPane.showInputDialog(null, "Informe o Apartamento;"));
+		for(Despesa d: despesas) {
+			if (d.getMes()== selecmes && d.getAno()== selecano){
+				Double valorporap = d.getVal()/numeroAp();
+				totaldesp+=valorporap;
+			}
+		}
+		for (Apartamento a: apartamentos) {
+			if (a.getNum()== selecap) {
+				soma = a.getVal_fix()+totaldesp;
+			}
+		}
+		JOptionPane.showMessageDialog(null,"Esse Apartamento tem R$ "+soma+" de despesa total");
+	}
+	
+	public static boolean verificaNumeroAp(int numeroAp) {
+
+                for (Apartamento a : apartamentos) {
+
+                        if (a.getNum() == numeroAp) {
+                                return true;
+                        }
+                }
+
+                return false;
+        }
+}
